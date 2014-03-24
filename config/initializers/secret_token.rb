6fc9929e9b74184826d680a9d6bd23ff855a20d3
xdrm <xdrealmadrid@gmail.com>
2014-03-24 14:56:05 -0400

    first upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubTest::Application.config.secret_key_base = '7794371d3c13f27ed8abb05517adab55913e409390a02681076ac7770e8c27656e8d62a6c396a96dab7f0198b1e69ecf98533f639ff86793556064b81a090b6b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GithubTest::Application.config.session_store :cookie_store, key: '_github_test_session'
